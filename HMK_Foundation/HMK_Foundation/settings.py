388d361c4ba6053570a69fc9e6b0fe5a257d4916
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-12-01 00:15:47 +0600

    update

--- HMK_Foundation/HMK_Foundation/settings.py
+++ HMK_Foundation/HMK_Foundation/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = Path.joinpath(BASE_DIR,'templates')
+STATIC_DIR = Path.joinpath(BASE_DIR,"static")
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -108,8 +111,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- HMK_Foundation/HM_FoundationApp/views.py
+++ HMK_Foundation/HM_FoundationApp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    my_dict = { 'homepage':"hellooooo"}
+    my_dict = { 'homepage':""}
     return render(request,'index.html',context=my_dict)
      
