613ec846ed568b9149a7ad6d55d33602d1c6a1cc
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-12-03 00:48:44 +0600

    jjj

--- HMK_Foundation/HM_FoundationApp/admin.py
+++ HMK_Foundation/HM_FoundationApp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from HM_FoundationApp.models import Executive_Counsil
 # Register your models here.
+admin.site.register(Executive_Counsil)
--- HMK_Foundation/HM_FoundationApp/models.py
+++ HMK_Foundation/HM_FoundationApp/models.py
@@ -3,18 +3,22 @@ from django.db import models
 # Create your models here.
 class Executive_Counsil(models.Model):
     ec_name = models.CharField(max_length=264,unique=True)
+    ec_post = models.CharField(max_length=264,unique=True,null=True)
+    ec_number = models.CharField(max_length=264,unique=True,null=True)
     def __str__(self):
         return self.ec_name
-class EC_Post(models.Model):
-    name = models.ForeignKey('Executive_Counsil',
-    on_delete=models.CASCADE)
-    ec_post = models.CharField(max_length=264,unique=True)
-    def __str__(self):
         return self.ec_post
-class EC_Number(models.Model):
+        return self.ec_number
+# class EC_Post(models.Model):
+#     name = models.ForeignKey('Executive_Counsil',
+#     on_delete=models.CASCADE)
+#     ec_post = models.CharField(max_length=264,unique=True)
+#     def __str__(self):
+#         return self.ec_post
+# class EC_Number(models.Model):
     
-    post = models.ForeignKey('EC_Post',
-    on_delete=models.CASCADE)
-    ec_number = models.CharField(max_length=264,unique=True)
-    def __str__(self):
-        return self.ec_number 
+#     post = models.ForeignKey('EC_Post',
+#     on_delete=models.CASCADE)
+#     ec_number = models.CharField(max_length=264,unique=True)
+#     def __str__(self):
+#         return self.ec_number 
