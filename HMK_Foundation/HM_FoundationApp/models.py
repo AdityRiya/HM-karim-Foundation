ee607c5ad19ab5cc4dcb2564ac00e6c24fd5a33a
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-12-02 23:57:42 +0600

    update

--- HMK_Foundation/HM_FoundationApp/models.py
+++ HMK_Foundation/HM_FoundationApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Executive_Counsil(models.Model):
+    ec_name = models.CharField(max_length=264,unique=True)
+    def __str__(self):
+        return self.ec_name
+class EC_Post(models.Model):
+    name = models.ForeignKey(Executive_Counsil)
+    ec_post = models.CharField(max_length=264,unique=True)
+    def __str__(self):
+        return self.ec_post
+class EC_Number(models.Model):
+    post = models.ForeignKey(EC_Post)
+    ec_number = models.CharField(max_length=264,unique=True)
+    def __str__(self):
+        return self.ec_number
